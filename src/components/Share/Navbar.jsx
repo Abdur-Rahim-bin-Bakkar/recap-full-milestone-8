f60b01f73ed5a7c2fee9b86e5ab3fd581f073e3f
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-05-04 12:09:54 +0600

    install hero ui and gravity ui. add theme toggle system

--- src/components/Share/Navbar.jsx
+++ src/components/Share/Navbar.jsx
@@ -1,18 +1,45 @@
+"use client";
+import { useTheme } from 'next-themes';
 import Link from 'next/link';
 import React from 'react';
+import { Check, Power } from "@gravity-ui/icons";
+import { Switch } from "@heroui/react";
 
 const Navbar = () => {
+    const { theme, setTheme } = useTheme();
     return (
         <div className='flex justify-between h-15 shadow items-center'>
             <Link href={'/'} className='text-2xl font-bold'>Logo</Link>
-            <div className="flex gap-5 ">
+            <div className="flex gap-5 text-red-400">
                 <Link href={'/'}>Home</Link>
                 <Link href={'/'}>Home</Link>
                 <Link href={'/'}>Home</Link>
                 <Link href={'/'}>Home</Link>
             </div>
             <div className="">
-                
+                <button >
+                    <Switch onChange={() => setTheme(theme === "dark" ? "light" : "dark")} >
+                        {({ isSelected }) => (
+                            <>
+                                <Switch.Control
+                                    className={`h-[31px] w-[51px] bg-blue-500 ${isSelected ? "bg-cyan-500 shadow-[0_0_12px_rgba(6,182,212,0.5)]" : ""}`}
+                                >
+                                    <Switch.Thumb
+                                        className={`size-[27px] bg-white shadow-sm ${isSelected ? "ms-[22px] shadow-lg" : ""}`}
+                                    >
+                                        <Switch.Icon>
+                                            {isSelected ? (
+                                                <Check className="size-4 text-cyan-600" />
+                                            ) : (
+                                                <Power className="size-4 text-blue-600" />
+                                            )}
+                                        </Switch.Icon>
+                                    </Switch.Thumb>
+                                </Switch.Control>
+                            </>
+                        )}
+                    </Switch>
+                </button>
             </div>
         </div>
     );
